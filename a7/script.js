bb0a813f0fb121b760dcb5220b7ed345a17c6715
ManikantaGolla06 <manikantakirangolla@gmail.com>
2025-05-13 20:36:55 +0530

    okioi

--- a7/script.js
+++ a7/script.js
@@ -1,10 +1,37 @@
-function calculate() {
-    const num1 = parseFloat(document.getElementById('num1').value);
-    const num2 = parseFloat(document.getElementById('num2').value);
+const add = (a, b) => {
+    return a + b;
+  };
   
-    document.getElementById('addition').innerText = 'Addition: ' + (num1 + num2);
-    document.getElementById('subtraction').innerText = 'Subtraction: ' + (num1 - num2);
-    document.getElementById('multiplication').innerText = 'Multiplication: ' + (num1 * num2);
-    document.getElementById('division').innerText = 'Division: ' + (num2 !== 0 ? (num1 / num2) : 'Cannot divide by zero');
-  }
+  const subtract = (a, b) => {
+    return a - b;
+  };
   
+  const multiply = (a, b) => {
+    return a * b;
+  };
+  
+  const divide = (a, b) => {
+    return a / b;
+  };
+  
+  const dispResult = () => {
+    // resultDiv.style.display = "block"
+    resultDiv.style.left = "70%";
+  };
+  
+  const hideResult = () => {
+    //  resultDiv.style.display = "none"
+    resultDiv.style.left = "100%";
+  };
+  const calc = () => {
+    let n1 = parseFloat(document.getElementById("n1").value);
+    let n2 = parseFloat(document.getElementById("n2").value);
+    let r1 = add(n1, n2);
+    let r2 = subtract(n1, n2);
+    let r3 = multiply(n1, n2);
+    let r4 = divide(n1, n2);
+    dvAdd.innerHTML = "Addition: " + r1;
+    dvSubtract.innerHTML = `Subtraction: ${r2}`;
+    dvMultiply.innerHTML = `Multiplication: ${r3}`;
+    dvDivide.innerHTML = `Division: ${r4}`;
+  };
