bf866d5137094ce83d1f79886ee0da40edf6c499
ManikantaGolla06 <manikantakirangolla@gmail.com>
2025-05-13 19:49:29 +0530

    sffs

--- a7/script.js
+++ a7/script.js
@@ -1,7 +1,7 @@
 function calculate() {
     const num1 = parseFloat(document.getElementById('num1').value);
     const num2 = parseFloat(document.getElementById('num2').value);
-    
+  
     document.getElementById('addition').innerText = 'Addition: ' + (num1 + num2);
     document.getElementById('subtraction').innerText = 'Subtraction: ' + (num1 - num2);
     document.getElementById('multiplication').innerText = 'Multiplication: ' + (num1 * num2);
