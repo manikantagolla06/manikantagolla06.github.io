56c9d00c3f50e87b9876fb583ac96c451b674d99
ManikantaGolla06 <manikantakirangolla@gmail.com>
2025-05-13 19:37:03 +0530

    wssff

---
+++ a7/script.js
@@ -0,0 +1,10 @@
+function calculate() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    
+    document.getElementById('addition').innerText = 'Addition: ' + (num1 + num2);
+    document.getElementById('subtraction').innerText = 'Subtraction: ' + (num1 - num2);
+    document.getElementById('multiplication').innerText = 'Multiplication: ' + (num1 * num2);
+    document.getElementById('division').innerText = 'Division: ' + (num2 !== 0 ? (num1 / num2) : 'Cannot divide by zero');
+  }
+  
