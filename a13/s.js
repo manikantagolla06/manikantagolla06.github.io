77a9398f77709103f5d53f6157266c2e162c209c
ManikantaGolla06 <manikantakirangolla@gmail.com>
2025-05-16 19:52:40 +0530

    dcjn

--- a13/s.js
+++ a13/s.js
@@ -1,10 +1,9 @@
-let fruits = [
-  { id: 1, name: "Apple", price: 250 },
-  { id: 2, name: "Orange", price: 100 },
-  { id: 3, name: "Mango", price: 80 },
+const products = [
+  { id: 1, name: "Product1", price: 34 },
+  { id: 2, name: "Product2", price: 56 },
+  { id: 2, name: "Product3", price: 56 },
 ];
 
-fruits.forEach((element) => {
-    element.price+=5;
-  console.log(element.name+" "+element.price);
+products.map(product => {
+  console.log(product.name, product.price + 5);
 });
