89b369416a726f8ab9b09d05d1156f33859a13d2
ManikantaGolla06 <manikantakirangolla@gmail.com>
2025-05-16 19:31:47 +0530

    dsfd

---
+++ a12/n.js
@@ -0,0 +1,9 @@
+let fruits = [
+  { id: 1, name: "Apple", price: 250 },
+  { id: 2, name: "Orange", price: 100 },
+  { id: 3, name: "Mango", price: 80 },
+];
+
+fruits.forEach((element) => {
+  console.log(element.name);
+});
