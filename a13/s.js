0b2e854415067b0dc13fbf4c4b86d02aa1fb440c
ManikantaGolla06 <manikantakirangolla@gmail.com>
2025-05-18 17:12:51 +0530

    helo

--- a13/s.js
+++ a13/s.js
@@ -7,3 +7,15 @@ const products = [
 products.map(product => {
   console.log(product.name, product.price + 5);
 });
+let fruits = [
+  { id: 1, name: "Apple", price: 250 },
+  { id: 2, name: "Orange", price: 100 },
+  { id: 3, name: "Mango", price: 80 },
+];
+
+fruits.forEach((element) => {
+    element.price+=5;
+  console.log(element.name+" "+element.price);
+});
+
+
