60234ba30c14eeee99a12e644e1c5d7077fbfaca
ManikantaGolla06 <manikantakirangolla@gmail.com>
2025-05-19 20:14:57 +0530

    sccs

--- p4/s.js
+++ p4/s.js
@@ -2,14 +2,25 @@
   { cart: "1234567890", pin: "1234", name: "John", balance: 0 },
   { cart: "1234567891", pin: "2345", name: "Cathy", balance: 0 },
 ];-*/
-function Submit() {
-  const number = document.getElementById('number').value;
-  const pin = document.getElementById('pin').value;
+const customers = [
+  { cart: "1234567890", pin: "1234", name: "John", balance: 0 },
+  { cart: "1234567891", pin: "2345", name: "Cathy", balance: 0 },
+];
 
+function login() {
+  const cardInput = document.getElementById("cardNumber").value.trim();
+  const pinInput = document.getElementById("pin").value.trim();
+  const errorMsg = document.getElementById("errorMsg");
 
-  if (number === "1234567890" && pin === "1234") {
-    document.getElementById('message').textContent = "Welcome Manikanta!";
+  const customer = customers.find(
+    (c) => c.cart === cardInput && c.pin === pinInput
+  );
+
+  if (customer) {
+    document.querySelector(".atm-box").style.display = "none";
+    document.getElementById("welcomeBox").style.display = "block";
+    document.getElementById("userName").textContent = customer.name;
   } else {
-    document.getElementById('message').textContent = "Invalid number or pin.";
+    errorMsg.textContent = "Invalid card number or PIN.";
   }
 }
