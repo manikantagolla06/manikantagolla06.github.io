4207c7445f700ac28024233f9dc3784e58e0024e
ManikantaGolla06 <manikantakirangolla@gmail.com>
2025-05-19 20:02:12 +0530

    kidfmd

--- p4/s.js
+++ p4/s.js
@@ -1,4 +1,15 @@
-const customers = [
+/*const customers = [
   { cart: "1234567890", pin: "1234", name: "John", balance: 0 },
   { cart: "1234567891", pin: "2345", name: "Cathy", balance: 0 },
-];
+];-*/
+function handleSubmit() {
+  const number = document.getElementById('number').value;
+  const pin = document.getElementById('pin').value;
+
+
+  if (number === "1234567890" && pin === "6969") {
+    document.getElementById('message').textContent = "Welcome Manikanta!";
+  } else {
+    document.getElementById('message').textContent = "Invalid number or pin.";
+  }
+}
